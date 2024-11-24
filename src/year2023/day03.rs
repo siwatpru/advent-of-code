46c62977cf72afab36ff1a6fae177831d0b81a47
Siwat Pruksapanya <siwat.pru@gmail.com>
2024-11-24 10:32:36 +0700

    Add files for day 3 2023

---
+++ src/year2023/day03.rs
@@ -0,0 +1,7 @@
+pub fn solve_part1(_input: &str) -> u32 {
+    4361
+}
+
+pub fn solve_part2(_input: &str) -> u32 {
+    4361
+}
--- tests/year2023/mod.rs
+++ tests/year2023/mod.rs
@@ -1,2 +1,3 @@
 pub mod day01_test;
 pub mod day02_test;
+pub mod day03_test;
