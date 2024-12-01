fb347de3df40b534ac3ba95e01c26939df1db9fe
Siwat Pruksapanya <siwat.pru@gmail.com>
2024-12-01 12:32:23 +0700

    Finish 2024 day 1 :)

--- src/year2024/day01.rs
+++ src/year2024/day01.rs
@@ -1,8 +1,45 @@
-pub fn solve_part1(_input: &str) -> usize {
-    // Your solution logic here
-    42
+use regex::Regex;
+
+fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+    let re = Regex::new(r"(\d+)\s+(\d+)").unwrap();
+
+    for line in input.lines() {
+        if let Some(cap) = re.captures(line) {
+            left.push(cap[1].parse::<i32>().unwrap());
+            right.push(cap[2].parse::<i32>().unwrap());
+        }
+    }
+    (left, right)
 }
-pub fn solve_part2(_input: &str) -> usize {
-    // Your solution logic here
-    84
+
+pub fn solve_part1(input: &str) -> i32 {
+    let (mut left, mut right) = parse_input(input);
+
+    left.sort();
+    right.sort();
+
+    let mut sum: i32 = 0;
+    for (index, l) in left.iter().enumerate() {
+        let r = right
+            .get(index)
+            .expect("left and right should be the same size");
+        let distance = (r - l).abs();
+        sum += distance;
+    }
+
+    sum
+}
+
+pub fn solve_part2(input: &str) -> i32 {
+    let (left, right) = parse_input(input);
+
+    let mut sum: i32 = 0;
+    for l in left.iter() {
+        let count = right.iter().filter(|&&item| item == *l).count() as i32;
+        sum += count * l;
+    }
+
+    sum
 }
--- tests/year2024/day01_test.rs
+++ tests/year2024/day01_test.rs
@@ -2,12 +2,22 @@ use aoc::year2024::day01;
 
 #[test]
 fn test_part1() {
-    let input = "line1\nline2\nline3";
-    assert_eq!(day01::solve_part1(input), 42);
+    let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+    assert_eq!(day01::solve_part1(input), 11);
 }
 
 #[test]
 fn test_part2() {
-    let input = "line1\nline2\nline3";
-    assert_eq!(day01::solve_part2(input), 84);
+    let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+    assert_eq!(day01::solve_part2(input), 31);
 }
