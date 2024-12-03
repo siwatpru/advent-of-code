a3bb3cf7a658892087793eae251bcc10df57ba1b
Siwat Pruksapanya <siwat.pru@gmail.com>
2024-12-03 18:48:12 +0700

    Add 2024 day 03 part 1

--- src/year2024/day03.rs
+++ src/year2024/day03.rs
@@ -1,26 +1,33 @@
 use regex::Regex;
 
-fn parse_input(input: &str) -> Vec<Vec<i32>> {
-    let mut rows = Vec::new();
-    let re = Regex::new(r"(\d+)(?:\s+|$)").unwrap();
+fn parse_input(input: &str) -> Vec<(i32, i32)> {
+    let mut pairs = Vec::new();
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
+    let re2 = Regex::new(r"\d{1,3}").unwrap();
 
     for line in input.lines() {
-        let mut row = Vec::new();
         for cap in re.captures_iter(line) {
-            row.push(cap[1].parse::<i32>().unwrap());
-        }
-        if !row.is_empty() {
-            rows.push(row);
+            let mut cap2 = re2.captures_iter(&cap[0]);
+            if let (Some(first), Some(second)) = (cap2.next(), cap2.next()) {
+                let num1 = first[0].parse::<i32>().unwrap();
+                let num2 = second[0].parse::<i32>().unwrap();
+                pairs.push((num1, num2));
+            }
         }
     }
 
-    rows
+    pairs
 }
 
 pub fn solve_part1(input: &str) -> i32 {
-    161
+    let pairs = parse_input(input);
+    let mut sum = 0;
+    for pair in pairs {
+        sum += pair.0 * pair.1;
+    }
+    sum
 }
 
-pub fn solve_part2(input: &str) -> i32 {
+pub fn solve_part2(_input: &str) -> i32 {
     161
 }
